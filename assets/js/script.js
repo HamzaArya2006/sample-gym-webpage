0b64e3cbd1dc6fb4e6267eaade84ad27405235c1
HamzaArya2006 <hamzaarya123@gmail.com>
2025-03-01 12:25:24 +0430

    some changes made
    sidebar made for 768px width

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// here is the function that opens our sidebar
+function showSidebar(){
+    document.querySelector('.sidebar').style.display = 'flex';
+}
+// here is the function that closes our sidebar
+function closeSidebar(){
+    document.querySelector('.sidebar').style.display = 'none';
+}
